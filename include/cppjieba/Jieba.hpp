95f16631361dbb7822966960b9c341e539d8fb67
Harmen Landsmann <Harmen.Landsmann@riedel.net>
2024-04-14 09:08:54 +0200

    a lil bit of clean-up

--- include/cppjieba/Jieba.hpp
+++ include/cppjieba/Jieba.hpp
@@ -1,132 +1,164 @@
 #ifndef CPPJIEAB_JIEBA_H
 #define CPPJIEAB_JIEBA_H
 
-#include "QuerySegment.hpp"
 #include "KeywordExtractor.hpp"
+#include "QuerySegment.hpp"
+
+#include <cstddef>
+#include <string>
+#include <vector>
 
 namespace cppjieba {
 
-class Jieba {
- public:
-  Jieba(const string& dict_path, 
-        const string& model_path,
-        const string& user_dict_path, 
-        const string& idfPath, 
-        const string& stopWordPath) 
-    : dict_trie_(dict_path, user_dict_path),
-      model_(model_path),
-      mp_seg_(&dict_trie_),
-      hmm_seg_(&model_),
-      mix_seg_(&dict_trie_, &model_),
-      full_seg_(&dict_trie_),
-      query_seg_(&dict_trie_, &model_),
-      extractor(&dict_trie_, &model_, idfPath, stopWordPath) {
-  }
-  ~Jieba() {
-  }
+class Jieba
+{
+public:
+    Jieba(const std::string& dict_path,
+          const std::string& model_path,
+          const std::string& user_dict_path,
+          const std::string& idfPath,
+          const std::string& stopWordPath)
+        : dict_trie_(dict_path, user_dict_path), model_(model_path), mp_seg_(&dict_trie_), hmm_seg_(&model_), mix_seg_(&dict_trie_, &model_), full_seg_(&dict_trie_), query_seg_(&dict_trie_, &model_), extractor(&dict_trie_, &model_, idfPath, stopWordPath)
+    {
+    }
 
-  struct LocWord {
-    string word;
-    size_t begin;
-    size_t end;
-  }; // struct LocWord
+    // virtual ~Jieba() = default;
 
-  void Cut(const string& sentence, vector<string>& words, bool hmm = true) const {
-    mix_seg_.Cut(sentence, words, hmm);
-  }
-  void Cut(const string& sentence, vector<Word>& words, bool hmm = true) const {
-    mix_seg_.Cut(sentence, words, hmm);
-  }
-  void CutAll(const string& sentence, vector<string>& words) const {
-    full_seg_.Cut(sentence, words);
-  }
-  void CutAll(const string& sentence, vector<Word>& words) const {
-    full_seg_.Cut(sentence, words);
-  }
-  void CutForSearch(const string& sentence, vector<string>& words, bool hmm = true) const {
-    query_seg_.Cut(sentence, words, hmm);
-  }
-  void CutForSearch(const string& sentence, vector<Word>& words, bool hmm = true) const {
-    query_seg_.Cut(sentence, words, hmm);
-  }
-  void CutHMM(const string& sentence, vector<string>& words) const {
-    hmm_seg_.Cut(sentence, words);
-  }
-  void CutHMM(const string& sentence, vector<Word>& words) const {
-    hmm_seg_.Cut(sentence, words);
-  }
-  void CutSmall(const string& sentence, vector<string>& words, size_t max_word_len) const {
-    mp_seg_.Cut(sentence, words, max_word_len);
-  }
-  void CutSmall(const string& sentence, vector<Word>& words, size_t max_word_len) const {
-    mp_seg_.Cut(sentence, words, max_word_len);
-  }
-  
-  void Tag(const string& sentence, vector<pair<string, string> >& words) const {
-    mix_seg_.Tag(sentence, words);
-  }
-  string LookupTag(const string &str) const {
-    return mix_seg_.LookupTag(str);
-  }
-  bool InsertUserWord(const string& word, const string& tag = UNKNOWN_TAG) {
-    return dict_trie_.InsertUserWord(word, tag);
-  }
+    struct LocWord
+    {
+        std::string word;
+        size_t begin;
+        size_t end;
+    }; // struct LocWord
 
-  bool InsertUserWord(const string& word,int freq, const string& tag = UNKNOWN_TAG) {
-    return dict_trie_.InsertUserWord(word,freq, tag);
-  }
+    void Cut(const std::string& sentence, std::vector<std::string>& words, bool hmm = true) const
+    {
+        mix_seg_.Cut(sentence, words, hmm);
+    }
 
-  bool DeleteUserWord(const string& word, const string& tag = UNKNOWN_TAG) {
-    return dict_trie_.DeleteUserWord(word, tag);
-  }
-  
-  bool Find(const string& word)
-  {
-    return dict_trie_.Find(word);
-  }
+    void Cut(const std::string& sentence, std::vector<Word>& words, bool hmm = true) const
+    {
+        mix_seg_.Cut(sentence, words, hmm);
+    }
 
-  void ResetSeparators(const string& s) {
-    //TODO
-    mp_seg_.ResetSeparators(s);
-    hmm_seg_.ResetSeparators(s);
-    mix_seg_.ResetSeparators(s);
-    full_seg_.ResetSeparators(s);
-    query_seg_.ResetSeparators(s);
-  }
+    void CutAll(const std::string& sentence, std::vector<std::string>& words) const
+    {
+        full_seg_.Cut(sentence, words);
+    }
 
-  const DictTrie* GetDictTrie() const {
-    return &dict_trie_;
-  } 
-  
-  const HMMModel* GetHMMModel() const {
-    return &model_;
-  }
+    void CutAll(const std::string& sentence, std::vector<Word>& words) const
+    {
+        full_seg_.Cut(sentence, words);
+    }
 
-  void LoadUserDict(const vector<string>& buf)  {
-    dict_trie_.LoadUserDict(buf);
-  }
+    void CutForSearch(const std::string& sentence, std::vector<std::string>& words, bool hmm = true) const
+    {
+        query_seg_.Cut(sentence, words, hmm);
+    }
 
-  void LoadUserDict(const set<string>& buf)  {
-    dict_trie_.LoadUserDict(buf);
-  }
+    void CutForSearch(const std::string& sentence, std::vector<Word>& words, bool hmm = true) const
+    {
+        query_seg_.Cut(sentence, words, hmm);
+    }
 
-  void LoadUserDict(const string& path)  {
-    dict_trie_.LoadUserDict(path);
-  }
+    void CutHMM(const std::string& sentence, std::vector<std::string>& words) const
+    {
+        hmm_seg_.Cut(sentence, words);
+    }
 
- private:
-  DictTrie dict_trie_;
-  HMMModel model_;
-  
-  // They share the same dict trie and model
-  MPSegment mp_seg_;
-  HMMSegment hmm_seg_;
-  MixSegment mix_seg_;
-  FullSegment full_seg_;
-  QuerySegment query_seg_;
+    void CutHMM(const std::string& sentence, std::vector<Word>& words) const
+    {
+        hmm_seg_.Cut(sentence, words);
+    }
 
- public:
-  KeywordExtractor extractor;
+    void CutSmall(const std::string& sentence, std::vector<std::string>& words, size_t max_word_len) const
+    {
+        mp_seg_.Cut(sentence, words, max_word_len);
+    }
+
+    void CutSmall(const std::string& sentence, std::vector<Word>& words, size_t max_word_len) const
+    {
+        mp_seg_.Cut(sentence, words, max_word_len);
+    }
+
+    void Tag(const std::string& sentence, std::vector<std::pair<std::string, std::string> >& words) const
+    {
+        mix_seg_.Tag(sentence, words);
+    }
+
+    std::string LookupTag(const std::string& str) const
+    {
+        return mix_seg_.LookupTag(str);
+    }
+
+    bool InsertUserWord(const std::string& word, const std::string& tag = UNKNOWN_TAG)
+    {
+        return dict_trie_.InsertUserWord(word, tag);
+    }
+
+    bool InsertUserWord(const std::string& word, int freq, const std::string& tag = UNKNOWN_TAG)
+    {
+        return dict_trie_.InsertUserWord(word, freq, tag);
+    }
+
+    bool DeleteUserWord(const std::string& word, const std::string& tag = UNKNOWN_TAG)
+    {
+        return dict_trie_.DeleteUserWord(word, tag);
+    }
+
+    bool Find(const std::string& word)
+    {
+        return dict_trie_.Find(word);
+    }
+
+    void ResetSeparators(const std::string& s)
+    {
+        // TODO
+        mp_seg_.ResetSeparators(s);
+        hmm_seg_.ResetSeparators(s);
+        mix_seg_.ResetSeparators(s);
+        full_seg_.ResetSeparators(s);
+        query_seg_.ResetSeparators(s);
+    }
+
+    const DictTrie* GetDictTrie() const
+    {
+        return &dict_trie_;
+    }
+
+    const HMMModel* GetHMMModel() const
+    {
+        return &model_;
+    }
+
+    void LoadUserDict(const std::vector<std::string>& buf)
+    {
+        dict_trie_.LoadUserDict(buf);
+    }
+
+    void LoadUserDict(const std::set<std::string>& buf)
+    {
+        dict_trie_.LoadUserDict(buf);
+    }
+
+    void LoadUserDict(const std::string& path)
+    {
+        dict_trie_.LoadUserDict(path);
+    }
+
+private:
+    DictTrie dict_trie_;
+    HMMModel model_;
+
+    // They share the same dict trie and model
+    MPSegment mp_seg_;
+    HMMSegment hmm_seg_;
+    MixSegment mix_seg_;
+    FullSegment full_seg_;
+    QuerySegment query_seg_;
+
+public:
+    KeywordExtractor extractor;
 }; // class Jieba
 
 } // namespace cppjieba
